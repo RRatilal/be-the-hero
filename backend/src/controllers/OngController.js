ba3b2b07158cfd814bbd3840ba7aaee3c188109b
Rachid Ratilal <rachidratilal@gmail.com>
2020-03-27 21:41:30 +0200

    Adding test's with jest

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,11 +1,11 @@
-const crypto = require('crypto');
-const connection = require('../database/connection')
+const generateUnique = require('../utils/generateUniqueId');
+const connection = require('../database/connection');
 
 module.exports = {
     async create(req, res) {
         const { name, email, whatsapp, city, uf } = req.body;
 
-        const id = crypto.randomBytes(4).toString('HEX');
+        const id = generateUnique();
 
         await connection('ongs').insert({
             id,
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,3 @@
-const express = require('express');
-const routes = require('./routes');
-const cors = require('cors');
-const app = express();
+const app = require('./app');
 
-app.use(cors());
-app.use(express.json());
-app.use(routes);
-
-app.listen(3333);
+app.listen(3333)
